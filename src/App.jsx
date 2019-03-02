d5e31f1853404ab8a4edd172d32d914ca620baed
soldierforus <tbennett3434@gmail.com>
2019-03-01 17:16:35 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Swiss Army App!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
